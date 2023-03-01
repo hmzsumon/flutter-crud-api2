e43c18bf41cb521662b36ea7d171e3f27d6c9c8b
Zakaria <zaknetsum@gmail.com>
2023-03-01 18:23:51 +0600

    data

--- config/app.config.js
+++ config/app.config.js
@@ -1,5 +1,5 @@
 const MONGO_DB_CONFIG = {
-	DB: 'mongodb://localhost:27017/ecommerce',
+	DB: 'mongodb+srv://onexluck24:Su112200@cluster0.68qjnqt.mongodb.net/Flutter-api?retryWrites=true&w=majority',
 };
 
 module.exports = {
